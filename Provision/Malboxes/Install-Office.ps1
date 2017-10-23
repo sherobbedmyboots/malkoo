dd098001d207acd6753c82b2cb0c560c1409f5a4
sherobbedmyboots <drewstaples1@gmail.com>
2017-10-23 18:16:53 -0500

    update

--- Provision/Malboxes/Install-Office.ps1
+++ Provision/Malboxes/Install-Office.ps1
@@ -1,8 +1,22 @@
-<!-- Configure install options for MS Office Professional Plus 2013 -->
-<Configuration Product="ProPlus">
+$ToolsDir = "C:\Tools"
+if ([Environment]::osversion.Version.Major -eq 10){
+    try {
+        Mount-DiskImage -imagepath $ToolsDir\OfficeProfessionalPlus_x64_en-us.img 
+        $drive = (get-PSDrive | ? Description -eq "15.0.4420.1017").Root
+        cd $drive
+        .\setup.exe /config $ToolsDir\extra\config.xml
+    }
+    catch {
+        7z e $ToolsDir\OfficeProfessionalPlus_x64_en-us.img -o"$ToolsDir"\Office -aoa
+        $ToolsDir\Office\setup.exe  /config $ToolsDir\extra\config.xml
+    }
+}
 
-  <Display Level="none" CompletionNotice="no" SupressModal="yes" AcceptEula="yes"  />
+if ([Environment]::osversion.Version.Major -eq 6){
+    cd C:\Tools\OfficeProfessionalPlus_x64_en-us
+    .\setup.exe /config C:\Tools\extra\config.xml
+}
 
-  <Setting Id="SETUP_REBOOT" Value="Never"  />
 
-</Configuration>
+
+
