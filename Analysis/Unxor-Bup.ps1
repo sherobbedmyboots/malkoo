cf9b84cd7239d33e2ac7d248a21111482bc4c54d
sherobbedmyboots <drewstaples1@gmail.com>
2017-10-24 16:48:35 -0500

    update

--- Analysis/Unxor-Bup.ps1
+++ Analysis/Unxor-Bup.ps1
@@ -4,12 +4,29 @@ param(
 
 function Unxor-Bup{
     $key = "0x6a"
-    $output = $bup + ".out"
-    $bytes = [System.IO.File]::ReadAllBytes("$bup")
-    for($i=0; $i -lt $bytes.count ; $i++){
-        $bytes[$i] = $bytes[$i] -bxor $key
+    
+    write-host "[+] " -fore yellow -nonewline; Write-host "Unzipping bup file..."
+    7z e $bup | Out-Null
+    $files = @(".\File_0", ".\Details")
+
+    write-host "[+] " -fore yellow -nonewline; Write-host "Unxoring files..."
+    foreach ($file in $files){
+        $output = $file + ".bin"
+        $bytes = [System.IO.File]::ReadAllBytes($file)
+        for($i=0; $i -lt $bytes.count ; $i++){
+            $bytes[$i] = $bytes[$i] -bxor $key
+        }
+        [System.IO.File]::WriteAllBytes("$output", $bytes)
     }
-    [System.IO.File]::WriteAllBytes("$output", $bytes)
-    write-host "[+] " -fore green -nonewline; Write-host "File: " -nonewline; Write-host "$bup unxor'd and saved to $output"
+
+    $p = Select-String -path ".\Details.bin" -pattern 'OriginalName'
+    $o = ($p | %{$_ -split("\\")})[-1]
+    mv ".\File_0.bin" ".\$o"
+    mv ".\Details.bin" ".\Details.txt"
+    foreach ($file in $files){
+        rm -fo $file
+    }
+    write-host "[+] " -fore green -nonewline; Write-host "File extracted: " -nonewline; Write-host -fore green "    $o"
+    write-host "[+] " -fore green -nonewline; Write-host "File information: " -nonewline; Write-host -fore green "  Details.txt"
 }
 Unxor-Bup -bup $bup 
