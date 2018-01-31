ef33bf52f4ceb9bed29da550bf716d5ef993f239
sherobbedmyboots <drewstaples1@gmail.com>
2018-01-30 18:40:17 -0600

    training

--- Templates/html/files/hello.js
+++
@@ -1,8 +0,0 @@
-var x;
-var y = confirm("Hello there! You are very special!");
-if (y==true) {
-	x = "Wow! That was a GREAT click!";
-} else {
-	x = "Dang! You just cancelled that like a BOSS!";
-}
-alert(x);
