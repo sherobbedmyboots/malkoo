df9bf30bfb66d5a24a43dcf97defaf2f08c1e860
sherobbedmyboots <drewstaples1@gmail.com>
2018-03-19 13:28:46 -0500

    Add files via upload

---
+++ Provision/1.ps1
@@ -0,0 +1,34 @@
+
+Set-Location -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce'
+Set-ItemProperty -Path . -Name "2" -Value 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe "$env:TEMP\2.ps1"'
+
+choco install kb976932 -y
+Restart-Computer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Provision/5.ps1
@@ -0,0 +1,8 @@
+Write-Consle $host.version
+
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe "$env:TEMP\Post-CuckooSpin.ps1"
+
+
+
+
+
