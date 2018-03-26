1326d0daef90ba7f3f619baa0664664806dfa46e
sherobbedmyboots <drewstaples1@gmail.com>
2018-03-26 08:09:46 -0500

    Delete 4.ps1

--- Provision/Cuckoo/4.ps1
+++
@@ -1,7 +0,0 @@
-# Starts PowerShell5 install
-
-Set-Location -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce'
-Set-ItemProperty -Path . -Name "5" -Value 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe "C:\Tools\Post-SpinCuckoo.ps1"'
-choco install powershell -y
-Set-Content -Path C:\Tools\4.txt -Value 4
-Restart-Computer
