2b13b8c2189bbc6cfca8d7134bee5fe4f25bde75
sherobbedmyboots <drewstaples1@gmail.com>
2018-03-26 11:04:35 -0500

    Update Strip-ForCuckoo.ps1

--- Provision/Cuckoo/Strip-ForCuckoo.ps1
+++ Provision/Cuckoo/Strip-ForCuckoo.ps1
@@ -1,45 +1,106 @@
-function Strip-ForCuckoo
+function Main
 {
     Param($Path)
-        
-    # Load application
-    Write-Host -Fore Green "Creating Outlook object..."
+
+    # Extract attachments and save in .\Out
+    createOutDir
     $outlook1 = New-Object -ComObject Outlook.Application
+    $files = Get-ChildItem $Path *.msg | select -exp FullName
+    $files | %{
+        extractAttAndWrite($_)
+    }
+
+    # Extract links from attachments
+    $outlook2 = New-Object -ComObject Outlook.Application
+    $atts = Get-ChildItem Out *.msg | select -exp FullName
+    $results = @()
+    $atts | %{
+        $links = extractLinks($_)
+        $results += New-Object -TypeName psobject -Property @{EmailName=$_.split('\')[-1]; Links=$links; LinkCount=$links.count}
+    }
+
+    # Write results to Links.txt
+    writeLinksFile($results)
+}
+
+function createOutDir(){
     if (Test-Path Out) {
         Remove-Item -Recurse -Force Out | Out-Null
     }
     New-Item -ItemType 'Directory' -Path Out -Force | Out-Null
-        
-    # Extract attachments
-    Write-Host -Fore Green "Extracting attachments..."
-    $files = Get-ChildItem $Path *.msg | select FullName
-    $files | % {
-        $msgFn = $_.FullName
-        $msg = $outlook1.CreateItemFromTemplate($msgFn)
-        $msg.Attachments | % {
-                
-            # Give attachment filename
-            $attFn = $msgFn -replace '\.msg$', " - Attachment - $($_.FileName)"
+}
 
-            # Save attachment and move to Out directory
-            $_.SaveAsFile($attFn)
-            mv $attFn 'Out\'
-        }
-    }
+function extractAttAndWrite($msgFile) {
     
-    # Extract Links From Attachments
-    Write-Host -Fore Green "Extracting Links from Attachments..."
-    $links = @()
-    $outlook2 = New-Object -ComObject Outlook.Application
-    $attachments = Get-ChildItem -Path '.\Out' *.msg | select FullName
-    $attachments | % {
-        $msgFn = $_.FullName
-        $msg = $outlook2.CreateItemFromTemplate($msgFn)
-        $rlink = $msg.Body | select-string -pattern '(ht|f)tp(s?)\:\/\/[\p{N}\p{L}]([-.\w]*[\p{N}\p{L}])*(:(0-9)*)*(\/?)([\p{L}\p{N}\-\.\?\,\/\\\+&amp;%\$#_]*)' | % {$_.Matches} | %{$_.Value}
-        if ($rlink) { $links += $rlink.trim('<>') }
-    }
-    if ($links) {
-        Set-Content -Path 'Out\Links.txt' -Value $links
+    Write-Host -Fore Green "[+] " -NoNewLine; Write-Host Reading: $msgFile
+    $msg = $outlook1.CreateItemFromTemplate($msgFile)
+    $msg.Attachments | % {
+        
+        # Give attachment filename
+        $attFn = $(pwd).path + '\Out\' + $_.FileName
+
+        # Save attachment in Out directory
+        $_.SaveAsFile($attFn)
     }
 }
-Strip-ForCuckoo -Path ($Path)
+
+function extractLinks($msgFile) {
+
+    $msg = $outlook2.CreateItemFromTemplate($msgFile)
+    $links = searchForUrl($msg.body)
+    $links = $links | select -Unique
+    $links | %{
+        $sanUrl = desensitizeUrl($_)
+        $_ = $sanUrl
+    }
+    return $links
+}
+
+function searchForUrl($body) {
+    $regex = '\b(?:(?:https?|ftp|file)://|www\.|ftp\.)(?:\([-A-Z0-9+&@#/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#/%=~_|$?!:,.]*\)|[A-Z0-9+&@#/%=~_|$])'
+    $hits = $body | select-string -AllMatches -pattern $regex  | % {$_.Matches} | %{$_.Value}
+    return $hits
+}
+
+function desensitizeUrl($url) {
+    $regex = '(\w+\.){1,}\w+@(\w+\.){0,}dhs\.gov'
+    $hits = $url | select-string -AllMatches -pattern $regex | %{$_.Matches} |%{$_.Value}
+    if ($hits) {
+        foreach ($hit in $hits){
+            if ($hit.contains('@')){
+                $url = $url.replace($hit, 'bubba.t.merica@freedom.com')
+            }
+            else {
+                $url = $url.replace($hit, 'mail.example.com')
+            }
+        }
+    }
+    return $url
+}
+
+function writeLinksFile($results) {
+    
+    $outpath = $(pwd).path + '\Out\Links.txt'
+    
+    Add-Content -path $outpath -value "###########################################`r`n"
+    Add-Content -path $outpath -value "##     Number of Links in Each Email     ##`r`n"
+    Add-Content -path $outpath -value "###########################################`r`n"
+    $results | select LinkCount, EmailName | sort -desc LinkCount | Out-File -Append $outpath -Encoding ASCII
+
+    Add-Content -path $outpath -value "`r`n###########################################`r`n"
+    Add-Content -path $outpath -value "##     List of Links Sorted By Email     ##`r`n"
+    Add-Content -path $outpath -value "###########################################`r`n`r`n"
+    $results | ? LinkCount -gt '0' | sort LinkCount | %{
+        Add-Content -path $outpath -value ("<<<<<  " + $_.EmailName + "  >>>>>")
+        Add-Content -path $outpath -value "`r`n-------------------------------------------------------------------------------------------------------`r`n"
+        Add-Content -path $outpath -value $_.Links
+        Add-Content -path $outpath -value "`r`n`r`n`r`n"  
+    }
+
+    Add-Content -path $outpath -value "`r`n############################################`r`n"
+    Add-Content -path $outpath -value "##  List of Unique Links in All Emails    ##`r`n"
+    Add-Content -path $outpath -value "############################################`r`n"
+    Add-Content -path $outpath -value $results.Links 
+}
+
+Main -Path ($Path)
