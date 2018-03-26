6fab5b7255c3cc67d7536981b6677cf4934f2bdc
sherobbedmyboots <drewstaples1@gmail.com>
2018-03-26 08:09:40 -0500

    Delete 3.ps1

--- Provision/Cuckoo/3.ps1
+++
@@ -1,7 +0,0 @@
-# Starts DotNet4.5 install
-
-Set-Location -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce'
-Set-ItemProperty -Path . -Name "4" -Value 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe "C:\Tools\4.ps1"'
-choco install dotnet4.5 -y
-Set-Content -Path C:\Tools\3.txt -Value 3
-Restart-Computer
