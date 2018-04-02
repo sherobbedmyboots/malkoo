fbfefc12dd9a611c210725f0032fbc6e6af8f5d8
sherobbedmyboots <drewstaples1@gmail.com>
2018-04-02 07:53:29 -0500

    Update 3.ps1

--- Provision/Cuckoo/tools/3.ps1
+++ Provision/Cuckoo/tools/3.ps1
@@ -2,6 +2,6 @@
 
 Set-Location -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce'
 Set-ItemProperty -Path . -Name "4" -Value 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe "C:\Tools\4.ps1"'
-choco install dotnet4.5 -y
+choco install dotnet4.5 -y --force
 Set-Content -Path C:\Tools\3.txt -Value 3
 Restart-Computer
