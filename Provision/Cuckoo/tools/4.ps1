d66d88767b57bc177865a56af2b513e12da18ac2
sherobbedmyboots <drewstaples1@gmail.com>
2018-04-02 07:53:46 -0500

    Update 4.ps1

--- Provision/Cuckoo/tools/4.ps1
+++ Provision/Cuckoo/tools/4.ps1
@@ -2,6 +2,6 @@
 
 Set-Location -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce'
 Set-ItemProperty -Path . -Name "5" -Value 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe "C:\Tools\Post-SpinCuckoo.ps1"'
-choco install powershell -y
+choco install powershell -y --force 
 Set-Content -Path C:\Tools\4.txt -Value 4
 Restart-Computer
