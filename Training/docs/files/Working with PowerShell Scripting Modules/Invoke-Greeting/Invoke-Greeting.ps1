80b7d2648e346181e2f59f1081d9ac81d54eab0d
sherobbedmyboots <22768523+sherobbedmyboots@users.noreply.github.com>
2020-07-07 14:15:45 -0500

    Add files via upload

---
+++ Training/docs/files/Working with PowerShell Scripting Modules/Invoke-Greeting/Invoke-Greeting.ps1
@@ -0,0 +1,13 @@
+﻿$names = @("Bill", "Ted", "Bob", "Ralph", "Eric")
+
+function getDayOfWeek {
+    return (Get-Date).DayOfWeek
+}
+
+function sayGreeting ($day, $name){
+    Write-Host Happy $day $name!
+}
+
+function DoIt {
+    $names | % { $day = getDayOfWeek; sayGreeting $day $_ }
+}
---
+++ Training/docs/files/Working with PowerShell Scripting Modules/Invoke-Greeting/Invoke-Greeting.psm1
@@ -0,0 +1,11 @@
+﻿function getDayOfWeek {
+    return (Get-Date).DayOfWeek
+}
+
+function sayGreeting ($day, $name){
+    Write-Host Happy $day $name!
+}
+
+function DoIt {
+    $names | % { $day = getDayOfWeek; sayGreeting $day $_ }
+}
