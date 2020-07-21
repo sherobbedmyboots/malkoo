1a894e7fa99b1be3d0a97da53820aed93e433ae4
sherobbedmyboots <22768523+sherobbedmyboots@users.noreply.github.com>
2020-07-21 13:52:36 -0500

    Update Prepare-PackageVM.ps1

--- Training/scripts/Prepare-PackageVM.ps1
+++ Training/scripts/Prepare-PackageVM.ps1
@@ -361,7 +361,7 @@ sdelete -z c:
 # Set network location private
 $networkListManager =
 [Activator]::CreateInstance([Type]::GetTypeFromCLSID([Guid]"{DCB00C01-570F-4A9B-8D69-199FDBA5723B}"))
-$connections = \$networkListManager.GetNetworkConnections() 
+$connections = $networkListManager.GetNetworkConnections() 
 $connections | % {$_.GetNetwork().SetCategory(1)}
 
 # Ensure PSRemoting is enabled
