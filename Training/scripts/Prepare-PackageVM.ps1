03bfbd173fdcd8a828e5bbfb9122ca297c1fa5bc
sherobbedmyboots <22768523+sherobbedmyboots@users.noreply.github.com>
2020-07-21 13:54:21 -0500

    Update Prepare-PackageVM.ps1

--- Training/scripts/Prepare-PackageVM.ps1
+++ Training/scripts/Prepare-PackageVM.ps1
@@ -359,8 +359,7 @@ Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows Defender" -Nam
 sdelete -z c:
 
 # Set network location private
-$networkListManager =
-[Activator]::CreateInstance([Type]::GetTypeFromCLSID([Guid]"{DCB00C01-570F-4A9B-8D69-199FDBA5723B}"))
+$networkListManager = [Activator]::CreateInstance([Type]::GetTypeFromCLSID([Guid]"{DCB00C01-570F-4A9B-8D69-199FDBA5723B}"))
 $connections = $networkListManager.GetNetworkConnections() 
 $connections | % {$_.GetNetwork().SetCategory(1)}
 
